3f7d7ca51f4833c63dd229cafe66f1f4b8fcad5f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-17 02:07:15 +0400

    Covered ru.

---
+++ tests/unit/ISO3166RUTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use arslanimamutdinov\ISOStandard3166\Country;
+use arslanimamutdinov\ISOStandard3166\ISO3166;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ISO3166RUTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166
+ */
+class ISO3166RUTest extends TestCase
+{
+    public function testRU(): void
+    {
+        $country = ISO3166::RU();
+
+        $this->assertInstanceOf(Country::class, $country);
+        $this->assertEquals('Russian Federation', $country->getName());
+        $this->assertEquals('RU', $country->getAlpha2());
+        $this->assertEquals('RUS', $country->getAlpha3());
+        $this->assertEquals('643', $country->getNumericCode());
+    }
+}
