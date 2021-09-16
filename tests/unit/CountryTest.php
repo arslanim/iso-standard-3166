e0e58fb2bafe09da517fe29e956387e7e831d3d6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-17 01:05:55 +0400

    Created test class.

---
+++ tests/unit/CountryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CountryTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166
+ */
+class CountryTest extends TestCase
+{
+}
