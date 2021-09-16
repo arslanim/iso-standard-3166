1bdf516b7daeee3ee9d642d63ddd05d3f0829a1b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-17 01:52:23 +0400

    Created iso test.

---
+++ tests/unit/ISO3166Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ISO3166Test
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166
+ */
+class ISO3166Test extends TestCase
+{
+}
