92bfa5c79dd84b56b92d099c4eba471f532c918d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-18 02:12:37 +0400

    Created test.

---
+++ tests/unit/standards/ISO3166AFTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit\standards;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ISO3166AFTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit\standards
+ * @group standard-3166
+ */
+class ISO3166AFTest extends TestCase
+{
+}
