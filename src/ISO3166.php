51d7a8005677cfde5ddfb73a6cca950741264f9d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-18 01:54:58 +0400

    Removed standards.

--- src/ISO3166.php
+++ src/ISO3166.php
@@ -14,40 +14,10 @@ abstract class ISO3166
 {
     private const COUNTRIES = [
         [
-            AttributeCodes::ATTRIBUTE_NAME => 'Austria',
-            AttributeCodes::ATTRIBUTE_ALPHA2 => 'AT',
-            AttributeCodes::ATTRIBUTE_ALPHA3 => 'AUT',
-            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '040',
-        ],
-        [
-            AttributeCodes::ATTRIBUTE_NAME => 'Germany',
-            AttributeCodes::ATTRIBUTE_ALPHA2 => 'DE',
-            AttributeCodes::ATTRIBUTE_ALPHA3 => 'DEU',
-            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '276',
-        ],
-        [
-            AttributeCodes::ATTRIBUTE_NAME => 'Netherlands',
-            AttributeCodes::ATTRIBUTE_ALPHA2 => 'NL',
-            AttributeCodes::ATTRIBUTE_ALPHA3 => 'NLD',
-            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '528',
-        ],
-        [
-            AttributeCodes::ATTRIBUTE_NAME => 'Poland',
-            AttributeCodes::ATTRIBUTE_ALPHA2 => 'PL',
-            AttributeCodes::ATTRIBUTE_ALPHA3 => 'POL',
-            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '616',
-        ],
-        [
             AttributeCodes::ATTRIBUTE_NAME => 'Russian Federation',
             AttributeCodes::ATTRIBUTE_ALPHA2 => 'RU',
             AttributeCodes::ATTRIBUTE_ALPHA3 => 'RUS',
             AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '643',
-        ],
-        [
-            AttributeCodes::ATTRIBUTE_NAME => 'United Kingdom of Great Britain and Northern Ireland',
-            AttributeCodes::ATTRIBUTE_ALPHA2 => 'GB',
-            AttributeCodes::ATTRIBUTE_ALPHA3 => 'GBR',
-            AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '826',
         ],
     ];
 
