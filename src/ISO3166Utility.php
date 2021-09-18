e6e258f7bda834c73369ae842da5ffe8612cc40f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-18 18:18:32 +0400

    Create ISO utility.

---
+++ src/ISO3166Utility.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166;
+
+class ISO3166Utility
+{
+    /**
+     * @return Country[]
+     */
+    public function getAll(): array
+    {
+        return ISO3166::getAll();
+    }
+}
