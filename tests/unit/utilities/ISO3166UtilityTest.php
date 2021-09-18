f2af43d2172d996f27d98039f2ad836a0c719c1d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-18 19:17:31 +0400

    Covered implementations.

--- tests/unit/utilities/ISO3166UtilityTest.php
+++ tests/unit/utilities/ISO3166UtilityTest.php
@@ -6,6 +6,8 @@ use arslanimamutdinov\ISOStandard3166\Country;
 use arslanimamutdinov\ISOStandard3166\ISO3166;
 use arslanimamutdinov\ISOStandard3166\ISO3166Utility;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionMethod;
 
 /**
  * Class ISO3166UtilityTest
@@ -24,6 +26,18 @@ class ISO3166UtilityTest extends TestCase
         parent::setUp();
 
         $this->ISO3166Utility = new ISO3166Utility();
+    }
+
+    public function testAllStandardsImplementedAlpha2Function(): void
+    {
+        $countries = ISO3166::getAll();
+
+        $standardUtilityClassFunctionNames = $this->getStandardUtilityClassFunctionNames();
+        $countriesStandardsAlpha2 = $this->getCountriesStadardsAlpha2($countries);
+
+        foreach ($countriesStandardsAlpha2 as $countryStandardAlpha2) {
+            $this->assertTrue(in_array($countryStandardAlpha2, $standardUtilityClassFunctionNames));
+        }
     }
 
     public function testGetAll(): void
@@ -64,5 +78,37 @@ class ISO3166UtilityTest extends TestCase
     public function testFailGetByNumericCode(): void
     {
         $this->assertNull($this->ISO3166Utility->getByNumericCode('foo'));
+    }
+
+    /**
+     * @param Country[] $countries
+     * @return string[]
+     */
+    private function getCountriesStadardsAlpha2(array $countries): array
+    {
+        return array_map(
+            function (Country $country): string {
+                return $country->getAlpha2();
+            },
+            $countries
+        );
+    }
+
+    private function getStandardUtilityClassFunctionNames(): array
+    {
+        return array_map(
+            function (ReflectionMethod $method): string {
+                return $method->getName();
+            },
+            $this->getStandardUtilityClassFunctions()
+        );
+    }
+
+    /**
+     * @return ReflectionMethod[]
+     */
+    private function getStandardUtilityClassFunctions(): array
+    {
+        return (new ReflectionClass(ISO3166Utility::class))->getMethods();
     }
 }
