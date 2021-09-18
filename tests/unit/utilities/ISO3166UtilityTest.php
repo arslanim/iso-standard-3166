515c344e1593524ebabe9a2499560bb7a5d371c3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-18 18:20:05 +0400

    Created test.

---
+++ tests/unit/utilities/ISO3166UtilityTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit\utilities;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ISO3166UtilityTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit\utilities
+ * @group standard-3166-utilities
+ */
+class ISO3166UtilityTest extends TestCase
+{
+}
