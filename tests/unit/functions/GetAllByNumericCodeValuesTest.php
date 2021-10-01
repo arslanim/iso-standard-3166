53c168eb3e84454391ef56614e72f099fac1a446
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-01 23:45:16 +0400

    Added test class.

---
+++ tests/unit/functions/GetAllByNumericCodeValuesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetAllByNumericCodeValuesTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166-functions
+ */
+class GetAllByNumericCodeValuesTest extends TestCase
+{
+}
