4fa585daaeb21e245fb6d9362d6df3275c8284e4
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-01 23:27:31 +0400

    Created test class.

---
+++ tests/unit/functions/GetAllByAlpha3ValuesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetAllByAlpha3ValuesTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166-functions
+ */
+class GetAllByAlpha3ValuesTest extends TestCase
+{
+}
