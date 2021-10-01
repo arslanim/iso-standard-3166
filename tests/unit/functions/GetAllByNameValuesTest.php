00b25cb0809b5e2e9d3a68a4779a470b4691ff59
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-01 23:54:41 +0400

    Added test class.

---
+++ tests/unit/functions/GetAllByNameValuesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetAllByNameValuesTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166-functions
+ */
+class GetAllByNameValuesTest extends TestCase
+{
+}
