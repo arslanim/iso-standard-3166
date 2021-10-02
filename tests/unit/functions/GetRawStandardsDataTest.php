5ea8778ae28c8daec219e7e6ca3a0f5d98e3bd51
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-10-02 23:38:40 +0400

    Created test class.

---
+++ tests/unit/functions/GetRawStandardsDataTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandard3166\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetRawStandardsDataTest
+ * @package arslanimamutdinov\ISOStandard3166\tests\unit
+ * @group standard-3166-functions
+ */
+class GetRawStandardsDataTest extends TestCase
+{
+}
